aca8aee2e6d7355e93e4c7f826371505a274ecaa
Nick Nance <nance.nick@gmail.com>
2016-03-07 06:28:19 -0800

    initial working web server with upload form

---
+++ index.js
@@ -0,0 +1,26 @@
+"use strict"
+
+const express = require("express")
+const exphbs = require("express-handlebars")
+const app = express()
+const multer = require("multer")
+const upload = multer({ dest: "uploads/" })
+const port = 3000
+
+app.engine(".hbs", exphbs({defaultLayout: "single", extname: ".hbs"}))
+app.set("view engine", ".hbs")
+app.set("view engine", "hbs")
+
+app.use(express.static("public"))
+app.use("/bootstrap", express.static(`${__dirname}/node_modules/bootstrap/dist`))
+app.use("/jquery", express.static(`${__dirname}/node_modules/jquery/dist`))
+
+app.get("/", (req, res) => res.render("index"))
+app.post("/upload", upload.array("images"), (req, res) => {
+  // req.files is array of `photos` files
+  // req.body will contain the text fields, if there were any
+  req.files.forEach((f) => console.dir(f))
+  res.render("index")
+})
+
+app.listen(port, () => console.log(`Listening on port ${port}`))
