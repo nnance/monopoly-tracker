3be67eb2d4cbaad302fd8ccf034434f99936abca
Nick Nance <nance.nick@gmail.com>
2016-03-07 21:56:54 -0800

    add ocr processing

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const express = require("express")
 const exphbs = require("express-handlebars")
 const app = express()
 const multer = require("multer")
+const tesseract = require("node-tesseract")
+
 const upload = multer({ dest: "uploads/" })
 const port = 3000
 
@@ -19,7 +21,16 @@ app.get("/", (req, res) => res.render("index"))
 app.post("/upload", upload.array("images"), (req, res) => {
   // req.files is array of `photos` files
   // req.body will contain the text fields, if there were any
-  req.files.forEach((f) => console.dir(f))
+  req.files.forEach((f) => {
+    console.dir(f)
+    tesseract.process(`${__dirname}/${f.path}`, (err, text) => {
+      if (err) {
+        console.error(err)
+      } else {
+        console.log(text)
+      }
+    })
+  })
   res.render("index")
 })
 
